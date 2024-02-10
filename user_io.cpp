09d090ab096a6568c3e8108f7148480018898a10
Mohd Danish <mddanish.cse@gmail.com>
2024-02-10 15:57:16 +0530

    Initial commit

---
+++ user_io.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << "x = " << x << endl
+         << "y = " << y << endl;
+    return 0;
+}

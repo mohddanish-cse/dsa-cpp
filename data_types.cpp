65c3f41093bc6d8ee7c8114bc79009dfa986cfa6
Mohd Danish <mddanish.cse@gmail.com>
2024-02-11 11:47:07 +0530

    Initial Commit

---
+++ data_types.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a;
+    char ch = '-';
+    cout << ch;
+    string str;
+    getline(cin, str);
+    cout << str << '\n';
+    return 0;
+}

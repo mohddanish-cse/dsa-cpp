938e43eb53783037a3e6f176b55d993905c89b45
Mohd Danish <mddanish.cse@gmail.com>
2024-02-11 19:27:27 +0530

    Initial commit

---
+++ loops.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    a = 4;
+    do
+    {
+        cout << "Execute atleast once.\n";
+    } while (a < 4);
+    return 0;
+}
